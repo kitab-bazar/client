decc71e8729e31658e65527a973775e09e2f6c05
barshathakuri <barshathakuri708@gmail.com>
2022-01-25 15:12:12 +0545

    Add cart page

--- app/views/CartPage/index.tsx
+++ app/views/CartPage/index.tsx
@@ -1,35 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, NumberInput, TextOutput } from '@the-deep/deep-ui';
+import {
+    gql,
+    useMutation,
+    useQuery,
+} from '@apollo/client';
 import { AiTwotoneDelete } from 'react-icons/ai';
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaHeart, FaShoppingCart } from 'react-icons/fa';
 
 import styles from './styles.css';
+import { CartListQuery, CartListQueryVariables } from '#generated/types';
 
-function WishListBook() {
+const CART_LIST = gql`
+    query CartList ($email: ID!, $page: Int!, $pageSize: Int!) {
+        cartItems (createdBy: $email, page: $page, pageSize: $pageSize){
+            results {
+                id
+                totalPrice
+                book {
+                id
+                title
+                image {
+                    url
+                }
+                authors {
+                    id
+                    name
+                }
+                price
+                }
+                quantity
+            }
+            grandTotalPrice
+            page
+            pageSize
+            totalCount
+        }
+    }
+`;
+
+const DELETE_CART_ITEMS = gql`
+mutation DeleteCartItems ($id: ID!) {
+    deleteCartItem(id: $id) {
+      errors
+      ok
+    }
+  }
+`;
+
+interface Author {
+    id: number;
+    name: string;
+}
+
+interface Image {
+    url: string;
+}
+
+interface Book {
+    id: number;
+    price: number;
+    authors: Author[];
+    quantity: number;
+    image: Image;
+    title: string;
+    cartId: number;
+    mutate: (id: number) => void;
+}
+
+function CartContent(props: Book) {
+    const { id, price, authors, image, title, quantity, mutate, cartId } = props;
+    const authorsDisplay = React.useMemo(() => (
+        authors?.map((d) => d.name).join(', ')
+    ), [authors]);
+
+    const deleteCartItem = (_id: number) => {
+        mutate(_id);
+    };
     return (
         <div className={styles.container}>
             <div className={styles.metaData}>
-                {/* <img
+                {image?.url ? (
+                    <img
                         className={styles.image}
                         src={image.url}
-                        alt={}
+                        alt={title}
                     />
                 ) : (
                     <div className={styles.noPreview}>
                         Preview not available
                     </div>
-                )} */}
+                )}
                 <Container
                     className={styles.details}
-                    heading="cart"
+                    heading={title}
                 >
                     <div className={styles.headerDescription}>
                         <TextOutput
                             label="author"
+                            value={authorsDisplay}
                         />
                         <TextOutput
                             label="Price (NPR)"
                             valueType="number"
+                            value={price}
                         />
                         <div className={styles.quantity}>
                             <TextOutput
@@ -38,7 +112,7 @@ function WishListBook() {
                             />
                             <NumberInput
                                 name="quantity"
-                                value={undefined}
+                                value={quantity}
                                 onChange={undefined}
                             />
                         </div>
@@ -49,16 +123,17 @@ function WishListBook() {
                         name={undefined}
                         onClick={undefined}
                         variant="secondary"
-                        icons={<FaShoppingCart />}
+                        icons={<FaHeart />}
                         autoFocus
                     >
-                        Add to cart
+                        Add to wish list
                     </Button>
                     <Button
                         name={undefined}
                         variant="secondary"
                         icons={<AiTwotoneDelete />}
                         autoFocus
+                        onClick={() => deleteCartItem(cartId)}
                     >
                         Remove
                     </Button>
@@ -69,10 +144,53 @@ function WishListBook() {
 }
 
 function CartPage() {
+    const [page, setPage] = useState<number>(1);
+    const [pageSize, setPageSize] = useState<number>(10);
+    const [email, setEmail] = useState<string>('admin@gmail.com');
+    const [deleteCartItem] = useMutation(DELETE_CART_ITEMS);
+
+    const { data, refetch, loading } = useQuery<
+        CartListQuery,
+        CartListQueryVariables
+    >(CART_LIST, {
+        variables: { page, pageSize, email },
+    });
+
+    const removeCartItem = (_id: number) => {
+        deleteCartItem({ variables: { id: _id } });
+        refetch();
+    };
     return (
-        <div className={styles.wishList}>
-            <WishListBook />
-        </div>
+        <>
+            <div className={styles.wishList}>
+                {!loading && data?.cartItems?.results
+                    && (
+                        <>
+                            {
+                                data.cartItems.results.map((_b: any) => (
+                                    <CartContent
+                                        id={_b.book.id}
+                                        title={_b.book.title}
+                                        image={_b.book.image}
+                                        price={_b.book.price}
+                                        quantity={_b.quantity}
+                                        authors={_b.book.authors}
+                                        mutate={removeCartItem}
+                                        cartId={_b.id}
+                                    />
+                                ))
+                            }
+                        </>
+                    )}
+            </div>
+            <Button
+                name={undefined}
+                variant="secondary"
+                autoFocus
+            >
+                Order
+            </Button>
+        </>
     );
 }
 
