c33859694b7bf3aebc04ac098024d858f3b795ee
barshathakuri <barshathakuri708@gmail.com>
2022-04-01 15:56:58 +0545

    add styling

--- app/views/EBook/index.tsx
+++ app/views/EBook/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Header, ListView } from '@the-deep/deep-ui';
+
 import EbookBar from '#components/EbookBar';
+
 import styles from './styles.css';
 
 function EBook() {
     return (
         <div className={styles.ebook}>
+            <Header
+                className={styles.pageHeader}
+                heading="E-Books"
+                spacing="loose"
+            />
+            {/* <ListView
+                className={styles.bookList}
+                data={undefined}
+                rendererParams={bookItemRendererParams}
+                renderer={BookItem}
+                keySelector={keySelector}
+                errored={!!bookError}
+                filtered={filtered}
+                pending={bookLoading}
+                pendingMessage={strings.pendingBookListMessage}
+                emptyMessage={strings.bookListEmptyMessage}
+                messageShown
+            /> */}
+            <EbookBar />
             <EbookBar />
         </div>
     );
