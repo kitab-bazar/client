4a9711722eaffd52efba8f8faf18e9e22a4991bc
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-01-18 10:32:14 +0545

    Add grade images

--- app/views/HomePage/GradeItem/index.tsx
+++ app/views/HomePage/GradeItem/index.tsx
@@ -3,9 +3,12 @@ import { _cs } from '@togglecorp/fujs';
 import { Link } from '@the-deep/deep-ui';
 
 import { FooterGradeOptionsQuery, BookGradeEnum } from '#generated/types';
+import Ecd from '#resources/img/ecd.png';
 import Grade1 from '#resources/img/grade1.png';
 import Grade2 from '#resources/img/grade2.png';
 import Grade3 from '#resources/img/grade3.png';
+import Grade4 from '#resources/img/grade4.png';
+import Grade5 from '#resources/img/grade5.png';
 
 import routes from '#base/configs/routes';
 
@@ -14,9 +17,12 @@ import styles from './styles.css';
 const gradePreviewMap: {
     [key in BookGradeEnum]: string;
 } = {
+    ECD: Ecd,
     GRADE_1: Grade1,
     GRADE_2: Grade2,
     GRADE_3: Grade3,
+    GRADE_4: Grade4,
+    GRADE_5: Grade5,
 };
 
 export interface Props {
